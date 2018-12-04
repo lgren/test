afd36c54d400fb89a461f0279f3d603fe463ae0b
Lgren <625552409@qq.com>
2018-12-04 15:22:07 +0800

    poi更新 excel读取最终优化

--- src/main/java/com/lgren/简单工厂模式/a_普通/Sender.java
+++
@@ -1,5 +0,0 @@
-package com.lgren.简单工厂模式.a_普通;
-
-public interface Sender {
-    void send();
-}
--- src/main/java/com/lgren/简单工厂模式/a_普通/SmsSender.java
+++
@@ -1,9 +0,0 @@
-package com.lgren.简单工厂模式.a_普通;
-
-public class SmsSender implements Sender {
-
-    @Override
-    public void send() {
-        System.out.println("This is smsSender class");
-    }
-}
